2fdb7e44385b0b08af1b76b12ffbcec0b6af5e26
bprlab <bprlab@DESKTOP-I75BABB>
2018-03-24 14:28:24 +0300

    asd

--- Hesaplamalar/src/main/java/Alistirmalar.java
+++ Hesaplamalar/src/main/java/Alistirmalar.java
@@ -1,4 +1,19 @@
 
 public class Alistirmalar {
-
+ public static void main (String[] args) {
+	 int sonuc = topla(19,25);
+	 System.out.println(sonuc);
+}
+ public static int topla (int sayi1, int sayi2) {
+	 return sayi1 + sayi2;
+ }
+ public static int carp (int sayi1, int sayi2) {
+	 return sayi1 *sayi2;
+ }
+ public static int cikar (int sayi1, int sayi2) {
+	 return sayi1-sayi2;
+	 }
+ public static int bol (int sayi1, int sayi2) {
+	 return sayi1 / sayi2;
+ }
 }
